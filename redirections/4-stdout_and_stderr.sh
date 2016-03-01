e14682a1e624437e9be8c0098da4e091b97ecc09
Chandler Mayo <chandler@chandlermayo.com>
2016-03-01 13:20:25 -0800

    Modified code to meet requirements

--- redirections/4-stdout_and_stderr.sh
+++ redirections/4-stdout_and_stderr.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "$2" > /tmp/4-stdout_and_stderr
-$1 2>> /tmp/4-stdout_and_stderr
+echo "$2" > /tmp/4-stdout_and_stderr & $1 2>> /tmp/4-stdout_and_stderr
 cat /tmp/4-stdout_and_stderr
